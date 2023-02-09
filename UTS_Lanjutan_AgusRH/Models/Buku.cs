6e72014de3a59f4ba57e4eb37c83cce36ccef351
whybredz1933 <ww4697175@gmail.com>
2023-02-09 23:58:01 +0700

    Add project files.

---
+++ UTS_Lanjutan_AgusRH/Models/Buku.cs
@@ -0,0 +1,15 @@
+﻿namespace UTS_Lanjutan_AgusRH.Models
+{
+    public enum Buku
+    {
+        Komik_Black_Clover,
+        Komik_Jujutsu_Kaisen,
+        Komik_One_Piece,
+        Novel_Andrea_Hirata,
+        Novel_JK_Rowling,
+        Novel_Tere_Liye,
+        Sejarah_Indonesia,
+        Sejarah_Yunani,
+        Sejarah_Jepang
+    }
+}
---
+++ UTS_Lanjutan_AgusRH/Models/Genre.cs
@@ -0,0 +1,32 @@
+﻿namespace UTS_Lanjutan_AgusRH.Models
+{
+    public class Genre
+    {
+        public Buku GetBuku { get; set; }
+        public Genre(Buku buku) 
+        {
+            GetBuku = buku;
+        }
+
+        public string Book
+        {
+            get
+            {
+                string getgenre = "";
+                if(GetBuku == Buku.Komik_Black_Clover || GetBuku == Buku.Komik_Jujutsu_Kaisen || GetBuku == Buku.Komik_One_Piece)
+                {
+                    getgenre = "Komik";
+                }
+                else if(GetBuku == Buku.Novel_Andrea_Hirata || GetBuku == Buku.Novel_JK_Rowling || GetBuku == Buku.Novel_Tere_Liye)
+                {
+                    getgenre = "Novel";
+                }
+                else if(GetBuku == Buku.Sejarah_Indonesia || GetBuku == Buku.Sejarah_Yunani || GetBuku == Buku.Sejarah_Jepang)
+                {
+                    getgenre = "Sejarah";
+                }
+                return getgenre;
+            }
+        }
+    }
+}
